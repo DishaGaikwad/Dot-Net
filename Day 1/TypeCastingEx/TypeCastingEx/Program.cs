fbce97a09bda50b1661bb6909d46c17c05e22641
DishaGaikwad <101590165+DishaGaikwad@users.noreply.github.com>
2025-12-16 19:55:27 +0530

    Add files via upload

---
+++ Day 1/TypeCastingEx/TypeCastingEx/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+class BoxingUnboxingEx {
+
+
+    static void Main(string[] args) {
+
+        int x = 100;     // value type
+        String str = "Dikshita";     //referance type
+        Object obj = new Object(); // 'Object' is the base type of all types in C#
+        obj = new object();
+
+        
+
+        // BOXING: value type (int) is wrapped into an object (reference type)
+        obj = x;
+        Console.WriteLine("Boxed value : " +obj);
+
+        // UNBOXING: extracting the value type back from the object
+        int y =(int)obj;
+        Console.WriteLine("Unboxed value : " +y);
+
+
+
+        // Conversion: turning int into string
+        str = Convert.ToString(x); //Converts the integer value into its string
+        Console.WriteLine(str);
+        
+        
+        String str2 = x.ToString();//Another way to convert the integer into a string using the ToString() method
+        Console.WriteLine(str2);
+    }
+
+  
+
+}
---
+++ Day 1/typeCasting/typeCasting/Program.cs
@@ -0,0 +1,18 @@
+﻿class TypeCastingEx {
+
+    static void Main(string[] args) {
+
+        //type casting
+        int intVariable = 100;
+        double doubleVariable = intVariable;
+        Console.WriteLine("Implicit casting int to double: " + doubleVariable );
+
+        double x = 568.57;
+        int i = (int)x;
+        Console.WriteLine("Explicit casting idouble to int: "+ i);
+
+
+
+    }
+
+}
