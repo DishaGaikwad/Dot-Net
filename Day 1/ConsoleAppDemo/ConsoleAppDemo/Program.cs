25322a2e06649c8cd62c0ee806f025c2bf974150
DishaGaikwad <101590165+DishaGaikwad@users.noreply.github.com>
2025-12-16 19:51:58 +0530

    Add files via upload

---
+++ Day 1/ConsoleAppDemo/ConsoleAppDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("HELLO SHARKS");
+
+YourClass obj= new YourClass();
+obj.MyMethod();
+public class YourClass {
+
+    public void MyMethod() {
+
+        Console.WriteLine("Hii Panda");
+    
+    }
+
+}
---
+++ Day 1/Demo/Demo/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+using System;
+
+class TypeCastingEx
+{
+    static void Main(string[] args)
+    {
+        // Boxing
+        int x = 12321;          // value type
+        object obj = x;         // boxing: int → object
+        Console.WriteLine("Boxing: " + obj);
+
+        // Unboxing
+        int a = (int)obj;       // unboxing: object → int
+        Console.WriteLine("Unboxing: " + a);
+
+        // Conversion example
+        string str1 = "456";
+        int p = Convert.ToInt32(str1);   // valid conversion string → int
+        Console.WriteLine("Converted string to int: " + p);
+
+       
+    }
+}
